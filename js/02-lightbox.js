9a09e60db3e6bf803255e36569606c7e7a941f70
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-15 20:02:02 +0100

    add SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,9 +7,24 @@ const gallery = document.querySelector(".gallery");
 const addImages = galleryItems
 .map((image)=>
 `<a class="gallery__item" href="${image.original}">
-<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+<img class="gallery__image" data-source="${image.original}" src="${image.preview}" alt="${image.description}" />
 </a>`
 )
 .join('');
 
 gallery.insertAdjacentHTML("beforeend", addImages);
+
+gallery.addEventListener("click", (e) => {
+    e.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="1280" height="720">
+`);
+
+  instance.show();
+  document.addEventListener("keydown", (e)=>{
+if(e.key==="Escape"){
+  instance.close();
+}
+  });
+let galleryLbox = new SimpleLightbox('.gallery a').next();
+});
