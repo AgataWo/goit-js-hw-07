4ead338a319f99bd6414c5b8f59e43bba333b28b
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-15 19:21:14 +0100

    Initial commit

---
+++ js/01-gallery.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const addImages = galleryItems
+.map((image)=>
+`<div class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    data-source="${image.original}"
+    alt="${image.description}"
+  />
+</a>
+</div>`)
+.join('');
+
+  gallery.insertAdjacentHTML("beforeend", addImages);
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(e.target);
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="1280" height="720">
+`);
+
+  instance.show();
+});
