6bc99bde0e7574845f8ed2cd6be57c001837edc6
Agata Wójcik <agata_wojcik@onet.pl>
2022-12-15 19:42:13 +0100

    add images

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const addImages = galleryItems
+.map((image)=>
+`<a class="gallery__item" href="${image.original}">
+<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+)
+.join('');
+
+gallery.insertAdjacentHTML("beforeend", addImages);
